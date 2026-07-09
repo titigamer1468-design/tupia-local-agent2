ca5811525b73c6146874aaa8f44d40044748b7e7
titigamer1468-design <titigamer1468@gmail.com>
2026-07-09 16:37:23 -0400

    Fix de arquitectura: Movido interceptor a la raíz como index.js oficial

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ export default {
     const url = new URL(request.url);
     let response = await env.ASSETS.fetch(request);
     
-    // Si una ruta da 404, responde con el index.html para mantener el modo SPA de React
+    // Si una ruta da 404, responde con el index.html para mantener la app viva
     if (response.status === 404) {
       const indexRequest = new Request(`${url.origin}/index.html`, request);
       response = await env.ASSETS.fetch(indexRequest);
     }
     
-    // 🔥 FORZADO DE CABECERAS REALES EN LA NUBE DE CLOUDFLARE 🔥
+    // 🔥 INYECCIÓN DE MEMORIA REAL PARA EL CELULAR 🔥
     const newHeaders = new Headers(response.headers);
     newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
     newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
